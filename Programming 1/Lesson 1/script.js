7fff026c7e6649d2d5a3ef05c78b8b224be87ae8
a.kolyan <a.kolyan@gratun.am>
2017-10-02 17:24:06 +0400

    preparation for second lesson

---
+++ Programming 1/Lesson 1/script.js
@@ -0,0 +1,68 @@
+/* //պարզ օրինակ փոփոխականների օգտագործման
+alert("hello");
+
+
+var a = 15;
+var b = 100;
+var c = a +b;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+var name = "Artavazd";
+console.log("Name is "+ name);
+*/
+
+/* //պարզ օրինակ զանգվածների օգտագործման
+var animals = ["horse" ,"lion" ,"eagle" ,"tiger"];
+console.log(animals);
+
+animals[4] = "cat";
+console.log(animals);
+
+animals[0] = "unicorn";
+console.log(animals);
+*/
+
+/* //օրինակ վերագրման օպերացիաների օգտագործման
+var z = 100;
+z+=200;
+console.log(z);
+
+var m = 150;
+m-=15;
+console.log(m);
+
+m+=20;
+console.log(m); //155
+*/
+/*
+m/=5;
+console.log(m); //31
+*/
+/*
+var m = 23;
+m%=5;
+console.log(m);
+*/
+
+
+//օրինակ զանգվածների համատեղ օգտագործման
+var days = ["երկ","երք","չքշ","հնգշ","ուբթ","շբթ","կիր"];
+
+console.log(days[1]);
+console.log(days[2]);
+console.log(days[3]);
+console.log(days[4]);
+console.log(days[5]);
+console.log(days[6]);
+
+//օրինակ վերագրման օպերացիաների և զանգվածների համատեղ օգտագործման
+var nums = [45,12,89,32,100];
+var s = nums[0] + nums[1] + nums[2];
+var s1=0;
+s1+=nums[0];
+s1+=nums[1];  
+
+
