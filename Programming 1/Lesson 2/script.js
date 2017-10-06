1c0335a8f279c6e41ff1b174c7a1c5de13695d12
hanuman <a.kolyan@hanuman.am>
2017-10-06 11:35:10 +0000

    added Lesson 2 and Turtle, preparation for 3-rd lesson

--- Programming 1/Lesson 2/script.js
+++ Programming 1/Lesson 2/script.js
@@ -1,2 +1,68 @@
+/* //Օրինակներ prompt, parseInt, parseFloat
+var a = prompt("Input Number I");
+var b = prompt("Input Number II");
+a = parseInt(a);
+b = parseFloat(b);
 
+var z = a+b;
+console.log(z);
+*/
 
+/* //if-elseի օգտագործման օրինակ
+var age = prompt("Input your age");
+age = parseInt(age);
+
+if(age<18)
+{
+    console.log("Welcome !!!");
+}
+else
+{
+    console.log("You are too old");
+}
+
+console.log("You are "+age+" old");
+*/
+
+/* //օրինակ "դասակարգիչ" 
+var word = prompt("Ներմուծիր կենդանու անուն");
+
+if(word == "կատու" || word == "բեգեմոտ"){
+    console.log(word+ "-ը կաթնասուն է");
+}
+else if(word == "օձ"){
+    console.log(word+ "-ը սողուն է");	
+}
+else if(word == "արծիվ"){
+    console.log(word+ "-ը թռչյուն է");
+}
+else{
+    console.log(word+ "ի մասին ինֆորմացիա չկա");	
+}
+*/
+
+/* //օրինակ բառարան
+var word = prompt("ներմուծիր անգլերեն բառ");
+
+if(word == "Variable"){
+    console.log(word + " Փոփոխական");
+}
+else if(word == "Declaration"){
+    console.log(word + " Հայտարարում");
+}
+else if(word == "Assignment"){
+    console.log(word + " Վերագրում");
+}
+else if(word == "Data types"){
+    console.log(word + " Տվյալների տիպեր");
+}
+else if(word == "Number"){
+    console.log(word + " Թվային տիպ");
+}
+else if(word == "String"){
+    console.log(word + " Տողային տիպ");
+}
+else{
+    console.log("թարգմանություն չկա");
+}
+*/
