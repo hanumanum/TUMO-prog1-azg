af3d880708a38fa7c862b7147768a9d271ebeefb
hanuman <a.kolyan@hanuman.am>
2017-10-10 07:25:58 +0000

    start of 4th lesson

--- Programming 1/Lesson 3/script.js
+++ Programming 1/Lesson 3/script.js
@@ -1,2 +1,29 @@
+var a = prompt("I թիվը");
+var op = prompt("ներմուծիր գործող․");
+var b = prompt("II թիվը");
+
+a = parseFloat(a);
+b = parseFloat(b);
+
+//արժե նաև ստուգել, 
+//արդյո՞ք a-ն և b-ն թվային տիպի են
+if(op == "+"){
+    console.log(a+b);
+}
+else if(op == "-"){
+    console.log(a-b);
+}
+else if(op == "*"){
+    console.log(a*b);
+}
+else if(op == "/" && b!=0){
+    console.log(a/b);
+}
+else if(op == "/" && b==0){
+    console.log("0-ի վրա չենք բաժանում");
+}
+else{
+    console.log("սխալ օպերատոր");
+}
 
 
--- Programming 1/Lesson 3/turtle/turtle.js
+++ Programming 1/Lesson 3/turtle/turtle.js
@@ -11,6 +11,45 @@ angle(angle)	   //անկյուն
 width(width)       //մատիտի հաստությունը(հաստություն)
 colour(r,g,b) 	   //ընտրել գույն()
 */
+var n = prompt("ներմուծիր անկյունների քանակը");
+var ank = 180-((n-2)/n)*180;
+var a = prompt("ներմուծիր կողմը");
 
-colour(255,0,0);
-forward(100);
+colour(84, 153, 199);
+goto(121,-81);
+if(n<=2){
+    console.log(n+" անկյուն գոյություն չունի");
+}
+else if(n==3){
+    forward(a);
+    left(ank);
+    forward(a);
+    left(ank);
+    forward(a);
+}
+else if(n==4){
+    forward(a);
+    left(ank);
+    forward(a);
+    left(ank);
+    forward(a);
+    left(ank);
+    forward(a);
+  
+}
+else if(n==5){
+    forward(a);
+    left(ank);
+    forward(a);
+    left(ank);
+    forward(a);
+    left(ank);
+    forward(a);
+    left(ank);
+    forward(a);
+}
+else{
+    console.log("ես չգիտեմ ինչպես գծել" + n + "-անկյուն");
+    console.log("սովորեցրու ինձ :)");
+}
+
